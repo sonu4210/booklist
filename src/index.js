6d753693b5d5379c6649b7a5bd330b446f52f2c1
sonu4210 <souravjha497@gmail.com>
2021-08-06 08:07:36 -0700

    booklist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books =[
+    { id:1,
+    img:'https://images-eu.ssl-images-amazon.com/images/I/51Yngd0TjFS._SX198_BO1,204,203,200_QL40_FMwebp_.jpg',
+title:'think and grow rich',
+author:'Napoleon Hill',
+},
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+{ id:2,
+    img:'https://images-eu.ssl-images-amazon.com/images/I/51r4E42VAkL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg',
+title:'as a man thinketh',
+author:'James Allen',
+},
+
+]
+
+function Booklist(){
+    return(
+        <section className='booklist'>
+            {books.map((book,index) =>{
+                return <Book key={book.id} {...book}></Book>;
+                
+            })}
+        </section>
+    );
+}
+
+const Book =({img,title,author}) =>{
+
+     const clickHandler= (e)=>{
+         console.log(e);
+         alert('book added to cart');
+     }
+
+    return(
+        <article className='book' onMouseHover ={()=>{
+            console.log(author);
+        }} >
+            <img src={img} alt=''/>
+            <h4>{title}</h4>
+            <h4>{author}</h4>
+            <button type='button' onClick={clickHandler}>Buy</button>
+        </article>
+
+        
+    );
+};
+
+
+ReactDOM.render(<Booklist /> ,document.getElementById("root"));
